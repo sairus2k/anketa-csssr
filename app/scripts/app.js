9cacd0a3c01de50f87531fa6fe5d0a770e12515a
Anton Savoskin <anton@sliday.com>
2016-10-27 21:44:34 +0700

    Complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,10 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+import textarea from '../blocks/about/about';
+import jsLevel from '../blocks/js-level/js-level';
 
 $(() => {
 	svg4everybody();
+	textarea();
+	jsLevel();
 });
